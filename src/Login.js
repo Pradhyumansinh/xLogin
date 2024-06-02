ee93a85b24f6449e1029117f6f3b84be5bf83209
pradhyumansinh <jadejapradhyumansinh545@gmail.com>
2024-06-02 14:18:04 +0530

    lable added

--- src/Login.js
+++ src/Login.js
@@ -28,8 +28,8 @@ const Login = () => {
                 <p>{successMsg && <p>Welcome, {username}!</p>}</p>
                 :
                 <form onSubmit={handleSubmit}>
-                    Username:<input type="text" required onChange={(e) => setUserName(e.target.value)} value={username} placeholder="username" /><br></br>
-                    Password:<input type="password" required onChange={(e) => setPassword(e.target.value)} value={password} placeholder="password" /><br></br>
+                    <label for="Username">Username</label>:<input type="text" required onChange={(e) => setUserName(e.target.value)} value={username} placeholder="username" id="Username" /><br></br>
+                    <label for="Password">Password</label>:<input type="password" required onChange={(e) => setPassword(e.target.value)} value={password} placeholder="password" id="Password" /><br></br>
                     <button type="submit">Submit</button>
                 </form>
             }
